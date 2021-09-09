31856919679e2b9d0b05f3737e5045feca1ba6a8
Haroldamen <haroldamen.moundoyimboungou2021@campus-eni.fr>
2021-09-09 17:00:20 +0200

    #1003 ADD : Telechargement photo et organisation css, creation Service, modification de service.yaml

--- src/Form/UpdateProfileType.php
+++ src/Form/UpdateProfileType.php
@@ -12,6 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\ResetType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -21,34 +22,35 @@ class UpdateProfileType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('pseudo')
-            ->add('prenom')
-            ->add('nom')
-            ->add('telephone')
-            ->add('mail')
+            ->add('pseudo', TextType::class, ['label'=>'Pseudo : '])
+            ->add('prenom', TextType::class, ['label'=>'Prénom : '])
+            ->add('nom', TextType::class, ['label'=>'Nom : '])
+            ->add('telephone', TextType::class, ['label'=>'Téléphone : '])
+            ->add('mail', TextType::class, ['label'=>'Email : '])
             ->add('Password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'The password fields must match.',
                 'options' => ['attr' => ['class' => 'password-field']],
                 'required' => true,
-                'first_options'  => ['label' => 'Password'],
-                'second_options' => ['label' => 'Confirmation']])
+                'first_options'  => ['label' => 'Mot de passe :'],
+                'second_options' => ['label' => 'Confirmation :']])
             ->add('site', EntityType::class, [
                 'class'=>Site::class,
-                'choice_label'=>'nom'])
-            ->add('maPhoto', FileType::class, [
+                'choice_label'=>'nom',
+                'label'=>'Site de rattachement :'])
+            ->add('maPhotoFileName', FileType::class, [
                 'mapped' => false,
                 'required' => false,
-                'label' => 'Ma Photo'])
+                'label' => 'Ma Photo : '])
 
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
+//            ->add('agreeTerms', CheckboxType::class, [
+//                'mapped' => false,
+//                'constraints' => [
+//                    new IsTrue([
+//                        'message' => 'You should agree to our terms.',
+//                    ]),
+//                ],
+//            ])
 //            ->add('Enregistrer',SubmitType::class)
 //            ->add('Annuler',ResetType::class)
         ;
